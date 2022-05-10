146a58bd2323b5b0b6084c6e2f4cd135ca7117dd
Mikhail <shadow_qneps@mail.ru>
2022-05-11 00:06:15 +0500

    Home task 13.2.1 Seasons v.2

--- Homework13_2/src/main/kotlin/Seasons.kt
+++ Homework13_2/src/main/kotlin/Seasons.kt
@@ -1,14 +1,17 @@
 class Seasons {
     fun getSeason(n: Int): String {
         if(n in 1..12){
+            val season: String
             if (n in 3..5)
-                return "Spring"
+                season = "Spring"
             else if  (n in 6..8)
-                return "Summer"
+                season = "Summer"
             else if(n in 9..11)
-                return "Autumn"
+                season = "Autumn"
             else
-                return "Winter"
+                season = "Winter"
+            println(season)
+            return season
         }
         else
             throw IllegalArgumentException("Значение должно быть в пределах от 1 до 12: $n")
